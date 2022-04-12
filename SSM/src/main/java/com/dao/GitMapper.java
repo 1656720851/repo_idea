59c3e5e98d724f8e4a9c2f7f6239b087649668b7
com <1656720851@qq.com>
2022-04-12 23:25:28 +0800

    git updat two

--- SSM/src/main/java/com/dao/GitMapper.java
+++ SSM/src/main/java/com/dao/GitMapper.java
@@ -11,4 +11,12 @@ public interface GitMapper {
     public  void test3();
     public  void test4();
     public  void test5();
+
+    public  void test6();
+    public  void test7();
+    public  void test8();
+    public  void test9();
+    public  void test10();
+    public  void test11();
+
 }
