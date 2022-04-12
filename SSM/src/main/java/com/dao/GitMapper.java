54c522429cf34ff16839430b183e7ab1d39ee3c8
1656720851 <71088529+1656720851@users.noreply.github.com>
2022-04-12 23:20:07 +0800

    Update GitMapper.java

--- SSM/src/main/java/com/dao/GitMapper.java
+++ SSM/src/main/java/com/dao/GitMapper.java
@@ -7,4 +7,8 @@ public interface GitMapper {
     public  void test();
 
     public  void test1();
+    public  void test2();
+    public  void test3();
+    public  void test4();
+    public  void test5();
 }
